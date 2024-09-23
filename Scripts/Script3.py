8499e0cbbda214db6c32376916b8274133b932cf
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-23 14:06:43 -0300

    EX_4_parte_3
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ Scripts/Script3.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+num1 = float(input('Primeira nota: '))
+num2 = float(input('Segunda nota: '))
+
+print('a media do aluno é {}'.format((num1 + num2)/2))
