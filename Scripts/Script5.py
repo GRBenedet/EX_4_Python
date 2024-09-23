4d0c53fe464df2b595d0b60c9bd5e08a517d6b62
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-23 14:39:38 -0300

    EX_4_parte_5
    Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.

---
+++ Scripts/Script5.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+num = int(input('Digite um numero: '))
+
+print('-'*15 , '\n{} X 1  = {} \n{} X 2  = {} \n{} X 3  = {} \n{} X 4  = {} \n{} X 5  = {} \n{} X 6  = {} \n{} X 7  = {} \n{} X 8  = {} \n{} X 9  = {} \n{} X 10 = {}\n'.format(num, num * 1 , num , num * 2 , num , num * 3 , num , num * 4 , num , num * 5 , num , num * 6 , num , num * 7 , num , num * 8 , num , num * 9 , num , num * 10) , '-'*15)
