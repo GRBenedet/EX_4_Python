6234e46324b01219e1eea54f0a359b36521ce327
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-24 12:59:20 -0300

    EX_4_parte_10
    Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.

---
+++ Scripts/Script10.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+cel = float(input('quantos graus Celcius esta hoje? '))
+
+print('A temperatura de {}°C corresponde a {}°F!'.format(cel , cel * 1.8 + 32))
